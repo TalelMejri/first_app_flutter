efecdbcb4b577f409e5a2c3816d3ab772c58add8
talel <talelmejri8@gmail.com>
2023-06-18 13:54:22 +0100

    add image

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,12 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.yellow,
         appBar: AppBar(
-          title: const Text('Hello man'),
+            title: const Text('Hello man'), backgroundColor: Colors.red[200]),
+        body: const Image(
+          image: NetworkImage(
+              'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/512px-HTML5_logo_and_wordmark.svg.png?20170517184425'),
         ),
       ),
     ),
