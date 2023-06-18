c9d290cb9eeb5c7dc10c743d362a7608505aa4d3
talel <talelmejri8@gmail.com>
2023-06-18 14:15:43 +0100

    add images locale

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,11 @@ void main() {
         appBar: AppBar(
             title: const Text('Hello man'), backgroundColor: Colors.red[200]),
         body: Center(
-          child: Image(
-            image: NetworkImage(
-                'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/512px-HTML5_logo_and_wordmark.svg.png?20170517184425'),
-          ),
+          // child: Image(
+          //   image: NetworkImage(
+          //       'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/512px-HTML5_logo_and_wordmark.svg.png?20170517184425'),
+          // ),
+          child: Image(image: AssetImage('images/pro.jpg')),
         ),
       ),
     ),
