efcac04f9c66334ceec9580d17aa00895e82313a
talel <talelmejri8@gmail.com>
2023-06-18 13:20:59 +0100

    learn Scaffold>AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text("hello"),
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Hello man'),
+        ),
       ),
     ),
   );
