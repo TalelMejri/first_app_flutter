92334c056f8509c7f4a9bcc904a714c6bafed118
talel <talelmejri8@gmail.com>
2023-06-18 14:28:35 +0100

    disable debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
 
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       home: Scaffold(
+//         backgroundColor: Colors.yellow,
+//         appBar: AppBar(
+//           title: const Text('Hello man'),
+//           backgroundColor: Colors.red[200],
+//         ),
+//         body: Center(
+//           // child: Image(
+//           //   image: NetworkImage(
+//           //       'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/512px-HTML5_logo_and_wordmark.svg.png?20170517184425'),
+//           // ),
+//           child: Image(image: AssetImage('images/pro.jpg')),
+//         ),
+//       ),
+//     ),
+//   );
+// }
+
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.yellow,
-        appBar: AppBar(
-            title: const Text('Hello man'), backgroundColor: Colors.red[200]),
-        body: Center(
-          // child: Image(
-          //   image: NetworkImage(
-          //       'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/512px-HTML5_logo_and_wordmark.svg.png?20170517184425'),
-          // ),
-          child: Image(image: AssetImage('images/pro.jpg')),
-        ),
-      ),
-    ),
-  );
+  runApp(MaterialApp(
+    home: Scaffold(backgroundColor: Colors.blue),
+    debugShowCheckedModeBanner: false,
+  ));
 }
