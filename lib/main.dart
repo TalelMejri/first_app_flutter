9d1df3a6f1ad720e4a528868697778d1efc899f8
talel <talelmejri8@gmail.com>
2023-06-18 14:06:33 +0100

    center Image

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,11 @@ void main() {
         backgroundColor: Colors.yellow,
         appBar: AppBar(
             title: const Text('Hello man'), backgroundColor: Colors.red[200]),
-        body: const Image(
-          image: NetworkImage(
-              'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/512px-HTML5_logo_and_wordmark.svg.png?20170517184425'),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/512px-HTML5_logo_and_wordmark.svg.png?20170517184425'),
+          ),
         ),
       ),
     ),
