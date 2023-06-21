347808ac96e1b33b6c3078410fd7a840a4c187ed
talel <talelmejri8@gmail.com>
2023-06-21 17:03:59 +0100

    StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -22,8 +22,15 @@ import 'package:flutter/material.dart';
 // }
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(backgroundColor: Colors.blue),
-    debugShowCheckedModeBanner: false,
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(backgroundColor: Colors.red),
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
